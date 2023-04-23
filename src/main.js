be67338945d5ac8df21c6a4b154bf16862e03907
Emzzy241 <emzzyoluwole@gmail.com>
2023-04-23 05:17:49 +0100

    updating my javascript file

--- src/main.js
+++ src/main.js
@@ -8,44 +8,6 @@ import './css/styles.css';
 // import { CryptoService, SearchFeature } from "./agency-service.js";
 
 
-    // // for the buttons responsiveness
-    // const navbar = document.querySelector(".navbar");
-    // const menuButton = document.querySelector(".menu");
-
-    // menuButton.addEventListener("click", ()=>{
-    //     navbar.classList.toggle("show");
-    //     menuButton.classList.add("fa-solid fa-x");
-    // });
-    
-    
-        // using vanilla and jQuery to make an awesome navigation
-    
-    
-        document.onscroll = () => {
-            if (window.scrollY > 0) {
-                $(".header").addClass("active");
-                // document.querySelector(".header").classList.add("active");
-            }
-            else {
-                // document.querySelector(".header").classList.remove("active");
-                $(".header").removeClass("active");
-            }
-        }
-    
-        document.onload = () => {
-            if (window.scrollY > 0) {
-                $(".header").addClass("active");
-                // document.querySelector(".header").classList.add("active");
-            }
-            else {
-                // document.querySelector(".header").classList.remove("active");
-                $(".header").removeClass("active");
-            }
-        }
-
-
-
-
 // import Swiper bundle with all modules installed -- I didn't use it here but I might be using it later on for other pages in the website
 import Swiper from 'swiper/bundle';
 // import styles bundle
@@ -94,37 +56,17 @@ dom.watch();
 // importing all images I used in application 
 
 
-import mineCrypto from "./assets/images/cryptoMine.jpg";
-
-import btcImage from "./assets/images/btcImage.png";
-import myOfferImg from "./assets/images/cryptoOffer.png";
-import stockOffer from "./assets/images/stockOffers.png";
-import teslaStock from "./assets/images/tslaStock.png";
-import teslaImage from "./assets/images/ttt.png";
-import microsoftStock from "./assets/images/msft3.svg";
-
-
-// using all images below
-import companyStock from "./assets/images/msftStock.jpg";
-import msftImage from "./assets/images/msft2.png";
-
-
-import eth from "./assets/images/eth.svg";
-import bnc from "./assets/images/bnc.png";
 
 // using all images above
 
-import secureCrypto from "./assets/images/secure.png";
-import agencyLogo from "./assets/images/agencyLogo2.png";
-import agencyLogo2 from "./assets/images/agencyLogo1.png";
-import excImage from "./assets/images/exchange.png";
-import stkExchange from "./assets/images/stExchange.svg";
-import bot from "./assets/images/bot2.svg";
 import pcOne from "./assets/images/pic-1.png";
 import pcTwo from "./assets/images/pic-2.png";
 import pcThree from "./assets/images/pic-3.png";
 import pcFour from "./assets/images/pic-4.png";
-import tethImg from "./assets/images/teth.png";
+
+
+import agencyLogo from "./assets/images/agencyLogo1.png";
+import agencyLogo2 from "./assets/images/agencyLogo2.png";
 
 
 import feedNaijaImg from "./assets/images/FeedNaija.png";
@@ -133,26 +75,15 @@ import InfluenceImg from "./assets/images/Influence.png";
 import cryptoImg from "./assets/images/Crypto.png";
 import JshoesImg from "./assets/images/Jshoes.png";
 import ChurchImg from "./assets/images/Church.png";
-
+import businessMarketImg from "./assets/images/businessMarketing.png"
+import businessDevelopImg from "./assets/images/businessShop.png"
+import businessBrandImg from "./assets/images/businessRise.png"
+import businessDesignImg from "./assets/images/businessDesign.png"
+import businessSupportImg from "./assets/images/cSupport2.png"
+import businessPhotoImg from "./assets/images/helpBusiness.png"
+import heroImg from "./assets/images/Responsive-Design2.jpg"
 
 import jsBadgeImg from "./assets/images/js-badge.svg";
-
-
-
-
-// a function to clear the fields when user searches for crypto
-function clearSearchFields() {
-    $("#cryp").val("");
-    $(".content-name").text("");
-    $(".content-id").text("");
-    $(".content-marketcapRank").text("");
-    $(".content-symbol").text("");
-    $(".error").text("");
-}
-
-
-
-
 
 
 $(document).ready(function () {
@@ -172,15 +103,6 @@ $(document).ready(function () {
     });
 
 
-
-    // let myBtcImage = $(".imgBTC");
-    // myBtcImage.attr("src", btcImage);
-
-    // let myStockOffer = $(".imgStock");
-    // myStockOffer.attr("src", stockOffer);
-
-    // let myTeslaStock = $(".tsla");
-    // myTeslaStock.attr("src", teslaStock);
 
     // using images below
     let myAgency = $(".agencyAbout");
@@ -207,59 +129,35 @@ $(document).ready(function () {
     
     let service6 = $(".service6");
     service6.attr("src", cryptoImg);
+    service5.attr("src", InfluenceImg);
+    
+    let develop = $(".develop");
+    develop.attr("src", businessDevelopImg);
+    
+    let marketing = $(".marketing");
+    marketing.attr("src", businessMarketImg);
+    
+    let branding = $(".branding");
+    branding.attr("src", businessBrandImg);
+    
+    let design = $(".design");
+    design.attr("src", businessDesignImg);
+    
+    let support = $(".support");
+    support.attr("src", businessSupportImg);
+    
+    let photography = $(".photography");
+    photography.attr("src", businessPhotoImg);
+    
+    let myHeroImg = $(".hero");
+    myHeroImg.attr("src", heroImg);
 
 
-    // let tslaImg = $(".dgCoin");
-    // // change image to doge
-    // tslaImg.attr("src", teslaImage);
-
-    // using images above
-
-
-
-    // let mineImg = $(".mine");
-    // mineImg.attr("src", mineCrypto);
-
-    // let exchangeImg = $(".exc");
-    // exchangeImg.attr("src", excImage);
-
-    // let stkExchangeImg = $(".stex");
-    // stkExchangeImg.attr("src", stkExchange);
-
-    // let courseImg = $(".cour");
-    // courseImg.attr("src", course);
-
-
-    // let walletImg = $(".wallet");
-    // walletImg.attr("src", wallet);
-
-    // let companyImg = $(".cs");
-    // companyImg.attr("src", companyStock);
-
-    // let picOneImg = $(".pc1");
-    // picOneImg.attr("src", pcOne);
-
-    // let picTwoImg = $(".pc2");
-    // picTwoImg.attr("src", pcTwo);
-
-    // let picThreeImg = $(".pc3");
-    // picThreeImg.attr("src", pcThree);
-
-    // let picFourImg = $(".pc4");
-    // picFourImg.attr("src", pcFour);
-
-    // let tetherImg = $(".tethCoin");
-    // tetherImg.attr("src", tethImg);
-
-    // let rippImg = $(".rippCoin");
-    // rippImg.attr("src", ripple);
-
-    // let dogeImg = $(".dgCoin");
-    // dogeImg.attr("src", doge);
+   
 
     // for the top image in app
     let topImg = $(".appImg");
-    topImg.attr("href", agencyLogo2);
+    topImg.attr("href", agencyLogo);
 
 
 
